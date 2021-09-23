3dc22793e7a465d4560567a0cd5d048be19ff959
crazylinuxer <kvirikroma@gmail.com>
2021-09-23 16:25:35 +0300

    minor improvements

--- src/main.c
+++ src/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
-// #include <argp.h>
 #include <sys/time.h>
+#include <stdlib.h>
 
 #include "heap.h"
 
-#define TESTS_COUNT 10
 
-
-int integer_comparator(const void* item1, const void* item2)
+int uint32_comparator(const void* item1, const void* item2)
 {
     return (*(uint32_t*)item1 >= *(uint32_t*)item2) - (*(uint32_t*)item1 <= *(uint32_t*)item2);
+}
+
+int uint64_comparator(const void* item1, const void* item2)
+{
+    return (*(uint64_t*)item1 >= *(uint64_t*)item2) - (*(uint64_t*)item1 <= *(uint64_t*)item2);
 }
 
 void get_random_data(void* storage, uint32_t n_bytes)
@@ -50,33 +53,43 @@ int64_t get_current_microsecond(void)
     return ((time.tv_usec) + (time.tv_sec * 1000000));
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    uint32_t data[65536];
-    int64_t microseconds_sum = 0;
-    for (uint32_t i = 0; i < TESTS_COUNT; i++)
+    if (argc != 3)
     {
-        get_random_data(data, sizeof(data));
+        fprintf(stderr, "Invalid args\n");
+        exit(1);
+    }
+    uint64_t items_count = 0;
+    uint64_t tests_count = 0;
+    sscanf(argv[1], "%lu", &items_count);
+    sscanf(argv[2], "%lu", &tests_count);
+    uint64_t* data = calloc(items_count, sizeof(uint64_t));
+    int64_t microseconds_sum = 0;
+    for (uint32_t i = 0; i < tests_count; i++)
+    {
+        get_random_data(data, items_count);
         int64_t start_microsecond = get_current_microsecond();
-        heap_inplace_sort(data, sizeof(data) / sizeof(uint32_t), sizeof(uint32_t), integer_comparator, true);
+        heap_inplace_sort(data, items_count, sizeof(uint64_t), uint64_comparator, true);
         int64_t microseconds_elapsed = get_current_microsecond() - start_microsecond;
-        if (!check_sorted_sequence(data, sizeof(data) / sizeof(uint32_t), sizeof(uint32_t), integer_comparator, true))
+        if (!check_sorted_sequence(data, items_count, sizeof(uint64_t), uint64_comparator, true))
         {
             printf("ERROR!\n");
-            _exit(1);
+            exit(1);
         }
         microseconds_sum += microseconds_elapsed;
         printf(
-            "Test %u: elapsed %ld.%ld milliseconds\n", i + 1,
+            "Test %u: elapsed %ld.%.3ld milliseconds\n", i + 1,
             microseconds_elapsed / 1000,
             microseconds_elapsed % 1000
         );
     }
-    int64_t microseconds_avg = microseconds_sum / TESTS_COUNT;
+    int64_t microseconds_avg = microseconds_sum / tests_count;
     printf(
-        "AVERAGE: %ld.%ld milliseconds\n",
+        "AVERAGE: %ld.%.3ld milliseconds\n",
         microseconds_avg / 1000,
         microseconds_avg % 1000
     );
+    free(data);
     return 0;
 }
